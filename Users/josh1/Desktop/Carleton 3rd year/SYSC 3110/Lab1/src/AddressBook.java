37b46c667649b3cb3b59d3f124f2d628a3ef8aeb
josh1 <joshuanoronha3@cmail.carleton.ca>
2022-09-18 22:16:29 -0500

    Added Tom and removed Tom in BuddyInfo.

---
+++ Users/josh1/Desktop/Carleton 3rd year/SYSC 3110/Lab1/src/AddressBook.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class AddressBook {
+
+    private final Map<String , BuddyInfo> address_book;
+
+    public AddressBook() {
+        address_book = new HashMap<>();
+    }
+
+    public void addBuddy(String name, BuddyInfo buddy){
+        this.address_book.put(name, buddy);
+
+    }
+    public void  removeBuddy(String name){
+        this.address_book.remove(name);
+
+    }
+
+    public static void main (String [] args){
+
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy("Tom",buddy);
+        addressBook.removeBuddy("Tom");
+
+        AddressBook Info = new AddressBook();
+
+        String per1_name = "Franky";
+        String per2_name = "Billy";
+
+        BuddyInfo per1 = new BuddyInfo(per1_name, "123 Dill rd", "324-555-6789");
+        BuddyInfo per2 = new BuddyInfo(per2_name, "479 Bob rd", "777-505-6759");
+
+        System.out.println("Address Book");
+
+
+        Info.addBuddy(per1_name,per1);
+        Info.addBuddy(per2_name,per2);
+
+        System.out.println("Size of Hashmap:" + Info.address_book.size());
+        System.out.println("Values of Hashmap:" +Info.address_book.values());
+        System.out.println("Keys in Hashmap:" +Info.address_book.keySet());
+
+        Info.removeBuddy("Franky");
+
+        System.out.println("Size of Hashmap:" + Info.address_book.size());
+        System.out.println("Values of Hashmap:" +Info.address_book.values());
+        System.out.println("Keys in Hashmap:" +Info.address_book.keySet());
+
+
+    }
+
+}
